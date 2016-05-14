bdd89071f5069b7585e8e04f406b201fbaf30d26
Andrzej Trzaska <atrzaska2@gmail.com>
2016-05-14 09:38:37 +0200

    WIP

--- virtualization.nix
+++ virtualization.nix
@@ -10,7 +10,6 @@
 
   virtualisation.docker.enable = true;
   virtualisation.virtualbox.host.enable = true;
-  nixpkgs.config.virtualbox.enableExtensionPack = true;
 
   nixpkgs.config.packageOverrides = pkgs: rec {
     qemu = pkgs.qemu.override { spiceSupport = true; };
