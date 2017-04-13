f04b4c195232f01c932a5419c3eb4d2c46983565
Andrzej Trzaska <atrzaska2@gmail.com>
2017-04-13 08:12:18 +0200

    Delete unused scripts

--- postgresql.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql95;
-  services.postgresql.authentication = "local all all ident";
-  services.postgresql.initialScript = ./postgresql.sql;
-}
